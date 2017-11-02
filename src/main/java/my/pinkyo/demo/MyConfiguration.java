7fb166d23b3ed129fdbc66f83a4e997f1f787c2c
pinkyo <yinkn@outlook.com>
2017-11-02 22:46:30 +0800

    Others#, add banner and upgrade sprint-boot version from 1.5.4 to 1.5.8

--- src/main/java/my/pinkyo/demo/MyConfiguration.java
+++ src/main/java/my/pinkyo/demo/MyConfiguration.java
@@ -1,12 +1,7 @@
 package my.pinkyo.demo;
 
-import org.eclipse.jetty.util.thread.ExecutorThreadPool;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by yinkn on 2017/7/9.
@@ -14,12 +9,4 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class MyConfiguration {
-    @Bean
-    public JettyEmbeddedServletContainerFactory factory() {
-        JettyEmbeddedServletContainerFactory factory =
-                new JettyEmbeddedServletContainerFactory();
-        factory.setThreadPool(new ExecutorThreadPool(
-                50, 200, 5, TimeUnit.MINUTES));
-        return factory;
-    }
 }
--- src/main/java/my/pinkyo/demo/dao/UserDao.java
+++ src/main/java/my/pinkyo/demo/dao/UserDao.java
@@ -7,6 +7,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * Created by yinkn on 2017/7/9.
  */
 public interface UserDao extends PagingAndSortingRepository<UserEntity, String> {
-
     UserEntity findByName(String name);
 }

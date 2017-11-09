65a244474826ae686db59bc42646d4891c687d01
pinkyo <yinkn@outlook.com>
2017-11-09 20:28:57 +0800

    Others#, fix doc.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # springboot-demo
+
+[![Travis](https://img.shields.io/travis/pinkyo/springboot-demo.svg)](https://travis-ci.org/pinkyo/springboot-demo)
+[![Coveralls github](https://img.shields.io/coveralls/github/pinkyo/springboot-demo.svg)](https://coveralls.io/github/pinkyo/springboot-demo)
+
+This a demo project for selft learning, don't take it as a prudution starter project.
--- src/main/java/my/pinkyo/demo/controller/TestController.java
+++ src/main/java/my/pinkyo/demo/controller/TestController.java
@@ -1,13 +1,19 @@
 package my.pinkyo.demo.controller;
 
-import my.pinkyo.demo.model.User;
-import my.pinkyo.demo.service.UserService;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotNull;
+import my.pinkyo.demo.model.User;
+import my.pinkyo.demo.service.UserService;
 
 /**
  * Created by pinkyo on 2017/7/9.
